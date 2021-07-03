0a628a8992a7f750a4f98c1c3d1aa6e3784b91e4
Sammaljihad <jsammal69@gmail.com>
2021-07-03 17:25:16 +0700

    1.ser

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    SAMMAL CLOTHES
+                    GALGIL CLOTHES
                 </div>
             </div>
         </div>
